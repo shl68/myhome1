0de7726671132d75841d9afc5933cc108a5ecba3
shl68 <105649474+shl68@users.noreply.github.com>
2023-04-11 21:34:31 +0900

    layout 구성

---
+++ src/main/java/com/example/myhome1/Myhome1Application.java
@@ -0,0 +1,13 @@
+package com.example.myhome1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myhome1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myhome1Application.class, args);
+	}
+
+}
